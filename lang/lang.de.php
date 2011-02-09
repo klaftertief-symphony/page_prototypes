98bf75ba1762cf10d7252c38cd2e9c1620257701
Jonas Coch <klaftertief@googlemail.com>
2011-02-09 15:18:54 +0100

    updated language file

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -7,71 +7,64 @@
 			'email' => 'jonas@klaftertief.de',
 			'website' => 'http://klaftertief.de'
 		),
-		'release-date' => '2010-09-05',
+		'release-date' => '2011-02-09'
 	);
-
 
 	/**
 	 * Page Prototypes
 	 */
 	$dictionary = array(
 
-		'Missing or wrong parameter.' => 
-		'Fehlende oder falsche Parameter',
-
-		'The page prototype you requested does not exist.' => 
-		'Die angeforderste Musterseite existiert nicht.',
-
-		'Page Prototypes' => 
-		'Musterseiten',
-
-		'Create a new page prototype' => 
-		'Erstelle eine neue Musterseite',
+		'A %1$s page' => 
+		'Eine %1$s Seite',
 
 		'Available Actions' => 
 		'Vorhandene Aktionen',
 
-		'Create a new page by copying the prototype' => 
-		'Erstellt eine neue Seite durch Kopieren der Musterseite',
-
 		'Copy' => 
 		'Kopieren',
 
-		'or' => 
-		'oder',
-
-		'Create a new page by referencing the prototype' => 
-		'Erstellt eine neue Seite durch Referenzieren der Musterseite',
-
-		'Reference' => 
-		'Referenzieren',
-
-		'The prototype you requested to edit does not exist.' => 
-		'Die angeforderste Musterseite existiert nicht.',
-
-		'Page prototype updated at %1$s. <a href="%2$s">Create another?</a> <a href="%3$s">View all Page Prototypes</a> <a href="%4$s">Create Page Copy from Prototype</a> <a href="%5$s">Create Page Reference from Prototype</a>' => 
-		'Musterseite aktualisiert um %1$s. <a href="%2$s">Eine neue erstellen?</a> <a href="%3$s">Zeige alle Musterseiten</a> <a href="%4$s">Erstelle Seite durch Kopieren</a> <a href="%5$s">Erstelle Seite durch Referenzieren</a>',
-
-		'Page prototype created at %1$s. <a href="%2$s">Create another?</a> <a href="%3$s">View all Page Prototypes</a> <a href="%4$s">Create Page Copy from Prototype</a> <a href="%5$s">Create Page Reference from Prototype</a>' => 
-		'Musterseite erstellt um %1$s. <a href="%2$s">Eine neue erstellen?</a> <a href="%3$s">Zeige alle Musterseiten</a> <a href="%4$s">Erstelle Seite durch Kopieren</a> <a href="%5$s">Erstelle Seite durch Referenzieren</a>',
-
-		'Edit Prototype Template' => 
-		'Bearbeite Vorlage der Musterseite',
+		'Create %1$s page' => 
+		'Erstelle eine %1$s Seite',
 
 		'Create Page Prototype' => 
 		'Erstelle Musterseite',
 
+		'Create a %1$s page' => 
+		'Erstelle eine %1$s Seite',
+
+		'Create a new page by copying the prototype' => 
+		'Erstellt eine neue Seite durch Kopieren der Musterseite',
+
+		'Create a new page by referencing the prototype' => 
+		'Erstellt eine neue Seite durch Referenzieren der Musterseite',
+
+		'Create a new page prototype' => 
+		'Erstelle eine neue Musterseite',
+
 		'Delete this Page Prototype' => 
 		'Lösche diese Musterseite',
 
-		'Prototype template could not be written to disk. Please check permissions on <code>/workspace/pages</code>.' => 
-		'Die Vorlage der Musterseite konnte nicht gespeichert werden. Bitte kontroliere die Benutzerrechte auf <code>/workspace/pages</code>.',
+		'Edit Prototype Confguration' => 
+		'Bearbeite die Konfiguration der Musterseite',
 
-		'A prototype template with that title already exists' => 
-		'Eine Musterseite mit diesem Namen existiert schon',
+		'Edit Prototype Template' => 
+		'Bearbeite Vorlage der Musterseite',
 
-		'A prototype template with that handle already exists' => 
-		'Eine Musterseite mit diesem Bezeichner existiert schon',
+		'Missing or wrong parameter.' => 
+		'Fehlende oder falsche Parameter',
+
+		'One or more prototype templates could not be deleted. Please check permissions on <code>/workspace/pages</code>.' => 
+		'Eine oder mehr Mustervorlagen konnte nicht gelöscht werden. Bitte kontroliere die Benutzerrechte auf <code>/workspace/pages</code>.',
+
+		'Page Prototype' => 
+		'Musterseite',
+
+		'Page Prototypes' => 
+		'Musterseiten',
+
+		'Prototype Settings' => 
+		'Einstellungen der Musterseite',
 
 		'Prototype Template could not be deleted because it does not exist.' => 
 		'Die Vorlage der Musterseite konnte nicht gelöscht werden, da sie nicht existiert.',
@@ -79,23 +72,14 @@
 		'Prototype could not be deleted because it is referenced by the following pages.' => 
 		'Die Musterseite konnte nicht gelöscht werden, da sie von den folgenden Seiten referenziert wird:',
 
-		'One or more prototype templates could not be deleted. Please check permissions on <code>/workspace/pages</code>.' => 
-		'Eine oder mehr Mustervorlagen konnte nicht gelöscht werden. Bitte kontroliere die Benutzerrechte auf <code>/workspace/pages</code>.',
+		'Reference' => 
+		'Referenzieren',
 
-		'Prototype Template updated at %s. <a href="%s">View all Page Prototypes</a>' => 
-		'Vorlage der Musterseite aktualisiert um %s. <a href="%s">Zeige alle Musterseiten</a>',
+		'Reference a %1$s page' => 
+		'Referenziere eine %1$s Seite',
 
-		'Edit Prototype Confguration' => 
-		'Bearbeite die Konfiguration der Musterseite',
-
-		'Create a %1$s page' => 
-		'Erstelle eine %1$s Seite',
-
-		'A %1$s page' => 
-		'Eine %1$s Seite',
-
-		'Create %1$s page' => 
-		'Erstelle eine %1$s Seite',
+		'The page prototype you requested does not exist.' => 
+		'Die angeforderste Musterseite existiert nicht.',
 
 		'The page prototype you requested to copy does not exist.' => 
 		'Die Musterseite, die du kopieren willst, existiert nicht',
@@ -103,16 +87,31 @@
 		'The page prototype you requested to edit does not exist.' => 
 		'Die Musterseite, die du bearbeiten willst, existiert nicht',
 
-		'Reference a %1$s page' => 
-		'Referenziere eine %1$s Seite',
+		'The prototype you requested to edit does not exist.' => 
+		'Die angeforderste Musterseite existiert nicht.',
 
-		'Prototype Settings' => 
-		'Einstellungen der Musterseite',
+		'or' => 
+		'oder',
 
-		'Page Prototype' => 
-		'Musterseite',
+		'%s Reference Prototype' => 
+		'%s Referenziere die Musterseite',
 
-		'Reference Prototype' => 
-		'Referenziere die Musterseite',
+		'Page Prototype updated at %1$s. <a href="%2$s" accesskey="c">Create another?</a> <a href="%3$s" accesskey="a">View all Page Prototypes</a> <a href="%4$s">Create Page Copy from Prototype</a> <a href="%5$s">Create Page Reference from Prototype</a>' => 
+		'Musterseite aktualisiert um %1$s. <a href="%2$s" accesskey="c">Eine neue erstellen?</a> <a href="%3$s" accesskey="a">Zeige alle Musterseiten</a> <a href="%4$s">Erstelle Seite durch Kopieren</a> <a href="%5$s">Erstelle Seite durch Referenzieren</a>',
+
+		'Page Prototype created at %1$s. <a href="%2$s" accesskey="c">Create another?</a> <a href="%3$s" accesskey="a">View all Page Prototypes</a> <a href="%4$s">Create Page Copy from Prototype</a> <a href="%5$s">Create Page Reference from Prototype</a>' => 
+		'Musterseite erstellt um %1$s. <a href="%2$s" accesskey="c">Eine neue erstellen?</a> <a href="%3$s" accesskey="a">Zeige alle Musterseiten</a> <a href="%4$s">Erstelle Seite durch Kopieren</a> <a href="%5$s">Erstelle Seite durch Referenzieren</a>',
+
+		'Prototype could not be written to disk. Please check permissions on <code>/workspace/pages</code>.' => 
+		'Die Vorlage der Musterseite konnte nicht gespeichert werden. Bitte kontroliere die Benutzerrechte auf <code>/workspace/pages</code>.',
+
+		'A prototype with that title already exists' => 
+		'Eine Musterseite mit diesem Namen existiert schon',
+
+		'A prototype with that handle already exists' => 
+		'Eine Musterseite mit diesem Bezeichner existiert schon',
+
+		'Page Prototype updated at %1$s. <a href="%2$s" accesskey="c">Create another?</a> <a href="%3$s" accesskey="a">View all Page Prototypes</a>' => 
+		'Musterseite aktualisiert um %1$s. <a href="%2$s" accesskey="c">Zeige alle Musterseiten</a> <a href="%3$s" accesskey="a">Zeige alle Musterseiten</a>',
 
 	);
